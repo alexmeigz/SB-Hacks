21668281c6f8ce0677cdace9d15c934bcb982d96
Joseph Ng <yuheho7749@gmail.com>
2020-01-10 22:29:27 -0800

    Updated crop.py

--- crop.py
+++ crop.py
@@ -2,35 +2,40 @@
 from PIL import Image 
 import cv2
   
-# Opens a image in RGB mode 
-im = Image.open("capture.jpg") 
-  
-# Size of the image in pixels (size of orginal image) 
-# (This is not mandatory) 
-width, height = im.size 
+webcamImgs = ["capture1.jpg", "capture2.jpg", "capture3.jpg", "capture4.jpg", "capture5.jpg", "capture6.jpg"]
 
-  
-# Setting the points for cropped image 
-left = height/8
-top = height/8
-right = 7 * height / 8
-bottom =  7* height / 8
-  
-# Cropped image of above dimension 
-# (It will not change orginal image) 
-cubeSide = im.crop((left, top, right, bottom)) 
-cubeSide.save("imageSide.jpg")
-# Shows the image in image viewer 
-#imageCube.show()
+for imgName in webcamImgs:
+  # Opens a image in RGB mode 
+  im = Image.open(imgName) 
+    
+  # Size of the image in pixels (size of orginal image) 
+  # (This is not mandatory) 
+  width, height = im.size 
 
-# Cuts the pictures into 9 different color tiles
-colorTileImage = [[], [], []]
+    
+  # Setting the points for cropped image 
+  left = height/8
+  top = height/8
+  right = 7 * height / 8
+  bottom =  7* height / 8
+    
+  # Cropped image of above dimension 
+  # (It will not change orginal image) 
+  cubeSide = im.crop((left, top, right, bottom)) 
 
-w, h = cubeSide.size
+  cubeSide.save("imageSide.jpg") # saves portion of the cube face only
 
-for i in range(3):
-  for j in range(3):
-    colorTileImage[i].append(cubeSide.crop(((i * w/3), (j * h/3), ((i + 1) * w/3), ((j + 1) * h/3))))
-    colorTileImage[i][j].save("colorTile-" + str(i) + "-" +str(j) + ".jpg")
+  # Shows the image in image viewer 
+  #imageCube.show()
+
+  # Cuts the pictures into 9 different color tiles
+  colorTileImage = [[], [], []]
+
+  w, h = cubeSide.size
+
+  for i in range(3):
+    for j in range(3):
+      colorTileImage[i].append(cubeSide.crop(((i * w/3), (j * h/3), ((i + 1) * w/3), ((j + 1) * h/3))))
+      colorTileImage[i][j].save("colorTile-" + str(i) + "-" +str(j) + ".jpg")
 
 
