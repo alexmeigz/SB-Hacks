3a2983ad1039ed700ba96f5633ac9a438d9dea9a
alexmeigz <alexmei@ucsb.edu>
2020-01-11 11:28:31 -0800

    color detection, rgb

--- colorDetectFunc.py
+++ colorDetectFunc.py
@@ -1,13 +1,14 @@
-'''
-import os 
+import os
+from rgb import *
+from google.cloud import vision
+import io
+
 GOOGLE_APPLICATION_CREDENTIALS = "/Users/alex/Desktop/app_credentials.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
-'''
 
 def detect_properties(path):
     """Detects image properties in the file."""
-    from google.cloud import vision
-    import io
+
     client = vision.ImageAnnotatorClient()
 
     with io.open(path, 'rb') as image_file:
@@ -17,13 +18,17 @@ def detect_properties(path):
 
     response = client.image_properties(image=image)
     props = response.image_properties_annotation
-    print('Properties:')
 
-    for color in props.dominant_colors.colors:
-        print('fraction: {}'.format(color.pixel_fraction))
-        print('\tr: {}'.format(color.color.red))
-        print('\tg: {}'.format(color.color.green))
-        print('\tb: {}'.format(color.color.blue))
-        print('\ta: {}'.format(color.color.alpha))
+    dominant = determineDominant(props.dominant_colors.colors)
+    return dominant
         
-        
+def determineDominant(colors):
+    dominant = None
+    fraction = 0
+    for color in colors:
+        if fraction < color.pixel_fraction:
+            fraction = color.pixel_fraction
+            dominant = RGB(color.color.red, color.color.green, color.color.blue)
+    print(dominant)
+    return dominant
+
