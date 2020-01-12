1dd66c4f48625d8fec98e1528709089e190efbbd
Joseph Ng <yuheho7749@gmail.com>
2020-01-11 19:32:03 -0800

    fitness algorithm of cube state

---
+++ fitness.py
@@ -0,0 +1,11 @@
+
+from rubix import *
+from cube import *
+
+def calcFitness(cube):
+
+  count = 0
+  for cubie in cube.allPieces:
+    if cube.isCorrectOrientation(cubie):
+      count += 1;
+  return count
